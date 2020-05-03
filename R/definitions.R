e3c5e367b253af27b0f72175551a6d6688f061f9
sofigdl <garciasof@gmail.com>
2020-05-03 23:38:03 +0200

    update 03.05.2020

--- R/definitions.R
+++ R/definitions.R
@@ -1,6 +1,6 @@
 #' @title Relevant definitions and forest facts
 #' @description This function provides some relevant definitions in REDD context and forest facts collected from un-redd.org. These are basic concepts that may change between the countries or projects.
-#' @param term character, the term to be explained. It must be one of the words from this list: forest, afforestation, reforestation, bamboo, redwoods, mangroves, importance, floods, droughts, deforestation rate, temperature.
+#' @param term character, the term to be explained. It must be one of the words from this list: forest, deforestation, degradation, afforestation, reforestation, bamboo, redwoods, mangroves, importance, floods, droughts, deforestation rate, temperature.
 #' @return A forest fact or definition of forest concepts
 #' @import stringr
 #' @export
@@ -10,32 +10,46 @@ definitions<- function(term="auto"){
 
   term1<-stringr::str_remove(term, " ")
   term.low<-stringr::str_to_lower(term1, locale = "en")
+  forest<-forest<-"'Forest is a large area of trees whose canopies cover at least 10 per cent of the sky spanning more than 0.5 hectares with trees higher than 5 meters and a canopy cover of more than 10 percent.' -UN-REDD"
+  defores<- "'Deforestation is the process of converting forest land to another land use.  In other words, the primary use of the land ceases to be forest'-United Nations Environment Programme, 2018"
+  deforate<-"'The Earth loses a forest area about the size of 40 football fields every minute.' -UN-REDD"
+  degra<-"'Forest degradation is the process of losing carbon stock from forest land – i.e. the land use remains forest, but the amount of carbon stock in the forest is reduced.'-United Nations Environment Programme, 2018"
+  affo<-"'Afforestation means the planting of trees on land which was never forested' -UN-REDD"
+  refo<-"'Reforestation means planting of trees on land which was forested before.' -UN-REDD"
+  bamboo<-"'Bamboo is a giant woody grass which is grown chiefly in the tropics. It is one of the fastest growing plants on Earth, with reported growth rates of 100 centimeters (39 inches) in 24 hours.' -UN-REDD"
+  redwoods<-"'Redwoods are the tallest trees in the world and can easily reach heights of 300 feet (91 meters).' -UN-REDD"
+  mang<-"'Mangroves are trees or shrubs which grows in tidal, chiefly tropical, coastal swamps, having numerous tangled roots that grow above the ground and form dense thickets.' -UN-REDD"
+  importance<-"'An estimated 1.6 billion people use forests for all or part of their livelihoods. Forests also play a crucial role in fighting catastrophic climate change by absorbing and storing massive amounts of CO2. Forests are home to 80% of the world’s terrestrial biodiversity.' -UN-REDD"
+  flood<-"'During heavy rains, trees reduce the risk of flooding. Experts say that woodland acts as a barrier to floodwater, while trees also prevent soil erosion, reducing sediment going into rivers and increasing water absorption into the ground.' -UN-REDD"
+  dry<-"'Forests can retain excess rainwater, prevent extreme run-offs (...). They can also help mitigate the effects of droughts by releasing water in the dry season, forests can also help provide clean water and mitigate the effects of droughts' -UN-REDD"
+  temp<-"'Trees could reduce temperatures in cities up to 8°C, lowering use of air conditioning and related emissions by up to 40 per cent.' -UN-REDD"
+  todas<-c(forest, defores, deforate, degra, affo, refo, bamboo, redwoods, mang, importance, flood, dry, temp)
 
-  if(term.low=="auto"){
-    def <- "Please, use a term from the list to get the definition or forest fact (forest, afforestation, reforestation), you can find the list in the description of the function"
+    if(term.low=="auto"){
+    def <- todas
   } else if (term.low== "forest"){
-    def <-"'Forest is a large area of trees whose canopies cover at least 10 per cent of the sky spanning more than 0.5 hectares with trees higher than 5 meters and a canopy cover of more than 10 percent.' -UN-REDD"
+    def <- forest
   } else if (term.low == "afforestation"){
-    def<-"'Afforestation means the planting of trees on land which was never forested' -UN-REDD"
+    def<-affo
   } else if (term.low == "reforestation"){
-    def<-"'Reforestation means planting of trees on land which was forested before.' -UN-REDD"
+    def<-refo
   } else if (term.low== "bamboo"){
-      def <-"'Bamboo is a giant woody grass which is grown chiefly in the tropics. It is one of the fastest growing plants on Earth, with reported growth rates of 100 centimeters (39 inches) in 24 hours.' -UN-REDD"
+      def <-bamboo
   } else if (term.low== "redwoods"){
-      def<-"'Redwoods are the tallest trees in the world and can easily reach heights of 300 feet (91 meters).' -UN-REDD"
+      def<-redwoods
   } else if (term.low == "mangroves"){
-      def<-"'Mangroves are trees or shrubs which grows in tidal, chiefly tropical, coastal swamps, having numerous tangled roots that grow above the ground and form dense thickets.' -UN-REDD"
+      def<-mang
   } else if (term.low == "importance"){
-    def <-"'An estimated 1.6 billion people use forests for all or part of their livelihoods. Forests also play a crucial role in fighting catastrophic climate change by absorbing and storing massive amounts of CO2. Forests are home to 80% of the world’s terrestrial biodiversity.' -UN-REDD"
+    def <-importance
   } else if (term.low == "floods"){
-    def<-"'During heavy rains, trees reduce the risk of flooding. Experts say that woodland acts as a barrier to floodwater, while trees also prevent soil erosion, reducing sediment going into rivers and increasing water absorption into the ground.' -UN-REDD"
+    def<-flood
   } else if (term.low == "droughts"){
-    def<-"'Forests can retain excess rainwater, prevent extreme run-offs (...). They can also help mitigate the effects of droughts by releasing water in the dry season, forests can also help provide clean water and mitigate the effects of droughts' -UN-REDD"
+    def<-dry
   } else if (term.low == "deforestationrate"){
-    def <-"'The Earth loses a forest area about the size of 40 football fields every minute.' -UN-REDD"
+    def <-deforate
   } else if (term.low == "temperature"){
-    def<-"'Trees could reduce temperatures in cities up to 8°C, lowering use of air conditioning and related emissions by up to 40 per cent.' -UN-REDD"
-  } else def<-"Please, use a term from the list to get the definition or forest fact(forest, afforestation, reforestation), you can find the list in the description of the function"
+    def<-temp
+  } else def<-"Please, use a term from the list to get the definition or forest fact(forest, afforestation, reforestation...), you can find the list in the description of the function or leave empty to read all"
 
   print(def)
 }
--- R/deforestation.R
+++ R/deforestation.R
@@ -1,19 +1,19 @@
 #' @title CO2 emissions due to deforestation
-#' @description This function calculates the CO2 emissions (in Ton CO2) related to deforestation (forestland that changes to other land cover).For a test, you can run the function without specifying values.
+#' @description This function calculates the CO2 emissions (in Ton CO2) related to deforestation (forestland that changes to other land cover).
 #' @param area numeric, the area (in hectares) of deforestation related to an specific land cover
 #' @param new_land_cover character, the new land cover of the area
 #' @param emission_factor numeric, the emission factor (in ton C/Hectare)
+#' @param error_ef numeric, the relative error of the emission factor in percentage (from 0-100)
 #' @param points numeric, number of sample points with this specific land cover change
-#' @param total_area numeric, the area (in hectares) of deforestation related to an specific land cover
+#' @param total_area numeric, the total area of study (in hectares)
 #' @param years numeric, number of years between the land cover change analysis
 #' @param total_points numeric, number of total sample points
 #' @return CO2 deforestation emissions (in Ton CO2) for a certain land cover class
 #' @import
 #' @export
 
-defor_emiss<-function(area="auto", new_land_cover="agriculture", emission_factor, points, total_area, years, total_points){
-
-  if(area="auto") {
+defor_emiss<-function(area="auto", new_land_cover="the new land cover", emission_factor, error_ef=0, points, total_area, years, total_points){
+  if(area=="auto") {
     wi<-points/total_points
     area<- wi*total_area
     Ei_per<-sqrt((wi*(1-wi))/(total_points-1))
@@ -22,12 +22,15 @@ defor_emiss<-function(area="auto", new_land_cover="agriculture", emission_factor
     conf_int_per<-confidence_interval/total_area
     ci_inf<-area-Ei_ha
     ci_sup<-area+Ei_ha
+    error<-confidence_interval/area
   }
 
   TonC<-emission_factor*area
   TonC_yr<-TonC/years
   fCO2<-3.666667
   TonCO2<-TonC_yr*fCO2
+  RE<-(((emission_factor*(error*area))+(error_ef*emission_factor)*area)/TonCO2)*100
 
-  print("The emissions of CO2 relate to deforestation due to", new_land_cover, "are", TonCO2, "TOnCO2/year.")
-  }
+  cat("The emissions of CO2 related to deforestation due to", new_land_cover, "are", TonCO2, "TOnCO2/year with a relative error of ", RE, "%.")
+}
+
