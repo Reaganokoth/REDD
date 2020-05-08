1e5eca56316782f826fe5462c69688f1fb34984e
sofigdl <garciasof@gmail.com>
2020-05-08 03:10:00 +0200

    example

--- R/deforestation.R
+++ R/deforestation.R
@@ -1,18 +1,21 @@
 #' @title CO2 emissions due to deforestation
 #' @description This function calculates the CO2 emissions (in Ton CO2) related to deforestation (forestland that changes to other land cover).
-#' @param area numeric, the area (in hectares) of deforestation related to an specific land cover
-#' @param error numeric, error of the area (0-1)
-#' @param new_land_cover character, the new land cover of the area
 #' @param emission_factor numeric, the emission factor (in ton C/Hectare)
 #' @param error_ef numeric, the relative error of the emission factor in percentage (from 0-100)
 #' @param points numeric, number of sample points with this specific land cover change
+#' @param total_points numeric, number of total sample points
 #' @param total_area numeric, the total area of study (in hectares)
 #' @param years numeric, number of years between the land cover change analysis
-#' @param total_points numeric, number of total sample points
+#' @param new_land_cover character, the new land cover of the area
+#' @param area numeric, the area (in hectares) of deforestation related to an specific land cover, if it is not calculated by the number of points
+#' @param error numeric, error of the area (0-1)
 #' @return CO2 deforestation emissions (in Ton CO2) for a certain land cover class
+#' @examples
+#' defor_emiss(emission_factor = 117.06, points = 100, total_area = 9985930, years = 10, total_points = 10414)
+#'
 #' @export
 
-defor_emiss<-function(area="auto", error=0, new_land_cover="the new land cover", emission_factor, error_ef=0, points, total_area, years, total_points){
+defor_emiss<-function( emission_factor, error_ef=0, points, total_area, years, total_points, new_land_cover="the new land cover", area="auto", error=0){
 
    #If it is not provided, calculates the area using the number of samples for an specific class
   if(area=="auto") {
